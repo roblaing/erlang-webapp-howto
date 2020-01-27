3ae7c3f56521a4f639edd1c551e0c7d19efcdd1f
roblaing <robert.laing@rocketmail.com>
2020-01-27 13:40:13 +0200

    Half-way with database section

---
+++ unit3/apps/unit3/priv/scripts/arts.js
@@ -0,0 +1,16 @@
+function validateForm() {
+  let valid = true;
+  if (document.getElementById('title').value.length === 0) {
+    document.getElementById('title_error').textContent = "Please enter a title";
+    valid = false;  
+  } else {
+    document.getElementById('title_error').textContent = "";
+  }
+  if (document.getElementById('art').value.length === 0) {
+    document.getElementById('art_error').textContent = "Pleaser enter some art";
+    valid = false;  
+  } else {
+    document.getElementById('mail_error').textContent = "";
+  }
+  return valid;
+}
