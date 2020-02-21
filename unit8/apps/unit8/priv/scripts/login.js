93c60fb00b37389391aae052d0a3573f3d5b70eb
roblaing <robert.laing@rocketmail.com>
2020-02-21 15:29:46 +0200

    Tying everything together into a blog

--- unit8/apps/unit8/priv/scripts/login.js
+++ unit8/apps/unit8/priv/scripts/login.js
@@ -1,35 +1,35 @@
 "use strict";
-const websocket = new WebSocket("ws://localhost:3030/blog");
 const loginButton = document.querySelector("#loginButton");
 
-async function checkId() {
-  var id = await getId();
-  var msg = { page: "login"
-            , user_id: id
-            , uuid: window.localStorage.getItem("uuid")
-            };
-  websocket.send(JSON.stringify(msg));  
-}
+loginButton.addEventListener("click", (event) => {
+  let websocket = new WebSocket("ws://localhost:3030/blog");
 
-loginButton.addEventListener("click", (event) => checkId());
-
-window.addEventListener("unload", (event) => {
-  websocket.close();
-});
-
-websocket.addEventListener("message", (event) => {
-  var msg = JSON.parse(event.data);
-  if (msg.uuid === false) {
-    document.querySelector('#login_error').textContent = "Your user name or password is incorrect";
-  } else {
-    window.localStorage.setItem("username", document.querySelector("#username").value);
-    window.localStorage.setItem("uuid", msg.uuid);
-    document.location.replace("/");
+  async function async_getId() {
+    let id = await getId();
+    let msg = { page: "login"
+              , user_id: id
+              , uuid: window.localStorage.getItem("uuid")
+              };
+    websocket.send(JSON.stringify(msg));
   }
-});
 
-websocket.addEventListener("error", (event) => {
-  window.alert('WebSocket error: ', event);
-});
+  websocket.addEventListener("open", (event) => async_getId());
 
+  websocket.addEventListener("message", (event) => {
+    let msg = JSON.parse(event.data);
+    if (msg.uuid === false) {
+      document.querySelector('#login_error').textContent = "Your user name or password is incorrect";
+    } else {
+      window.localStorage.setItem("username", document.querySelector("#username").value);
+      window.localStorage.setItem("uuid", msg.uuid);
+      websocket.close();
+      document.location.replace("/");
+    }
+  });
+
+  websocket.addEventListener("error", (event) => {
+    window.alert('WebSocket error: ', event);
+  });
+  
+});
 
--- unit8/apps/unit8/priv/scripts/signup.js
+++ unit8/apps/unit8/priv/scripts/signup.js
@@ -1,43 +1,43 @@
 "use strict";
-const websocket = new WebSocket("ws://localhost:3030/blog");
 const loginButton = document.querySelector("#signupButton");
 
-async function checkId() {
-  if (document.getElementById("password").value === document.getElementById("verify").value) {
-    let id = await getId();
-    let msg = {
-      page: "signup",
-      user_id: id,
-      username: document.querySelector("#username").value,
-      email: document.querySelector("#email").value,
-      uuid: window.localStorage.getItem("uuid")
-    };
-    websocket.send(JSON.stringify(msg));    
-  } else {
-    document.getElementById('error_verify').textContent = "Password does not match";
-    document.getElementById('error_username').textContent = "";
+signupButton.addEventListener("click", (event) => {
+  let websocket = new WebSocket("ws://localhost:3030/blog");
+
+  async function async_getId() {
+    if (document.querySelector("#password").value === document.querySelector("#verify").value) {
+      let id = await getId();
+      let msg = {
+          page: "signup",
+          user_id: id,
+          username: document.querySelector("#username").value,
+          email: document.querySelector("#email").value,
+          uuid: window.localStorage.getItem("uuid")
+        };
+      websocket.send(JSON.stringify(msg));    
+    } else {
+      document.querySelector("#error_verify").textContent = "Password does not match";
+      document.querySelector("#error_username").textContent = "";
+    }
   }
-}
+ 
+  websocket.addEventListener("open", (event) => async_getId());
 
-signupButton.addEventListener("click", (event) => checkId());
+  websocket.addEventListener("message", (event) => {
+    let msg = JSON.parse(event.data);
+    if (msg.uuid === false) {
+      document.querySelector("#error_username").textContent = "Sorry, that name is already taken. Please pick another.";
+    } else {
+      window.localStorage.setItem("username", document.querySelector("#username").value);
+      window.localStorage.setItem("uuid", msg.uuid);
+      websocket.close();
+      document.location.replace("/");
+    }
+  });
 
-window.addEventListener("unload", (event) => {
-  websocket.close();
+  websocket.addEventListener("error", (event) => {
+    window.alert('WebSocket error: ', event);
+
+  });
 });
-
-websocket.addEventListener("message", (event) => {
-  var msg = JSON.parse(event.data);
-  if (msg.uuid === false) {
-    document.querySelector("#error_username").textContent = "Sorry, that name is already taken. Please pick another.";
-  } else {
-    window.localStorage.setItem("username", document.querySelector("#username").value);
-    window.localStorage.setItem("uuid", msg.uuid);
-    document.location.replace("/");
-  }
-});
-
-websocket.addEventListener("error", (event) => {
-  window.alert('WebSocket error: ', event);
-});
-
 
